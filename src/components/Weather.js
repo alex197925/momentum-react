57cb3c8fd8c750663cfd3c67f3127f969c89aa83
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-11-07 12:04:17 +0100

    first commit

---
+++ src/components/Weather.js
@@ -0,0 +1,34 @@
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=antwerpen&units=imperial&appid=2d8027559ee4c08ed99ff31fa9fb7e6e";
+
+const Weather = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(data);
+
+  if (!data) return null;
+
+  return (
+    <div>
+      <p>{data.main.temp.toFixed(0)}</p>
+    </div>
+  );
+};
+
+export default Weather;
