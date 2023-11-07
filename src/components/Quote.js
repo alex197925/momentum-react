25140879f3d1180480fd064023e14b5273e9a862
Tolok Oleksandr <tolokoleksandr@Toloks-MacBook-Pro.local>
2023-11-07 13:22:35 +0100

    Fetchin Quots API data and dispalying random text and author

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,32 @@
-import React, {useState, useEffect} from 'react'
+/** @format */
 
-const url = ''
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = "https://type.fit/api/quotes";
 
 const Quote = () => {
-  return (
-    <div>Quote</div>
-  )
-}
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-export default Quote
+  if (!data) return null;
+  console.log(data);
+
+  return (
+    <div>
+      <p>{data[0].text}</p>
+      <p>- {data[0].author.substr(0, 13)}</p>
+    </div>
+  );
+};
+
+export default Quote;
